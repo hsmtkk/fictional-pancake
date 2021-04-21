7ca64d348479682dca0b94d4152913991a63dac2
alice <alice@example.com>
2021-04-21 00:39:56 +0000

    working fine

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 
+	"golang.org/x/net/ipv4"
 	"golang.org/x/net/icmp"
 )
 
@@ -14,11 +15,25 @@ func main() {
 		log.Fatal(err)
 	}
 	defer pc.Close()
-	dst, err := net.ResolveUDPAddr("192.168.11.1", "udp")
+	dst, err := net.ResolveUDPAddr("udp", "192.168.11.1:12345")
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = pc.WriteTo([]byte("hoge"), dst)
+	body := icmp.Echo {
+		ID: 0,
+		Seq: 0,
+		Data: []byte("hoge"),
+	}
+	msg := icmp.Message {
+		Type: ipv4.ICMPTypeEcho              ,
+		Code:0,
+		Body: &body,
+	}
+	msgBytes, err := msg.Marshal(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = pc.WriteTo(msgBytes, dst)
 	if err != nil {
 		log.Fatal(err)
 	}
